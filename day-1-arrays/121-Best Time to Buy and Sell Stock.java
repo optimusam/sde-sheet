8e3d96f3fb4d69778e89b424eb0cd44b235b7cb5
Sameer Giri <sameergiri1997@gmail.com>
2022-03-18 04:47:23 +0530

    solve best time to buy and sell stocks

---
+++ day-1-arrays/121-Best Time to Buy and Sell Stock.java
@@ -0,0 +1,13 @@
+import java.util.Stack;
+
+class Solution {
+    public int maxProfit(int[] prices) {
+        int profit = 0;
+        int minPrice = (int)1e9;
+        for(int price: prices) {
+            minPrice = Math.min(minPrice, price);
+            profit = Math.max(profit, price-minPrice);
+        }
+        return profit;
+    }
+}
